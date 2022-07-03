2cfd4bd0e8b15f1e72cb3d5099ac7cbb39169b01
Zchun <z.c.chun2003@gmail.com>
2022-07-03 13:46:16 -0500

    added inquirer and work skeleton

--- index.js
+++ index.js
@@ -2,3 +2,33 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./src/generateMarkdown.js')
+
+//array of questions
+const questions [
+    //project name
+
+    //description of project
+
+    //installation Instructions
+
+    //usage information
+
+    //contribution guidlines
+
+    //test instructions
+
+    //liscensing
+
+    //git username
+
+    //email
+]
+
+//function to write README
+function writeREADME(filename, data){}
+
+//function to initialize application
+function initialize(){}
+
+//call initialize function
+initialize();
